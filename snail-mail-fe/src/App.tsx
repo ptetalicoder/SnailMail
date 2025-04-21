1dcce49d11d29fc563830f0e8c63c342e74edf8c
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-04-21 09:45:05 -0400

    snail mail front end setup done

---
+++ snail-mail-fe/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+//Welcome to App.tsx! This is the main component of our React App
+//Any other components we create will rendered here before they are visible
+function App() {
+  
+
+  {/* the return() of a component is just the view. what the component looks like */}
+  return (
+    <div>
+      {/* Simple Top Navbar */}
+      <nav>
+        <h2>🐌 SnailMail 🐌</h2>
+      </nav>
+
+      {/* Render the InboxComponent */}
+
+    </div>
+  )
+}
+
+export default App
