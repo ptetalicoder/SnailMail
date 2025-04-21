aaba203f80e1b9340de867ba418bb6f5e5614368
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-04-21 13:33:20 -0400

    backend done

--- SnailMailBE/src/main/java/com/revature/SnailMailBE/controllers/MailController.java
+++ SnailMailBE/src/main/java/com/revature/SnailMailBE/controllers/MailController.java
@@ -2,10 +2,7 @@ package com.revature.SnailMailBE.controllers;
 
 import com.revature.SnailMailBE.models.Mail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,14 +19,33 @@ public class MailController {
         //In a real app, this would send a request to the database to get the user's inbox mail records
         //For now, we'll return a hardcoded list of Mail
         List<Mail> inbox = List.of(
-                new Mail("snail@snailmail.com", "me@snailmail.com", "Hey", "I am a snail"),
-                new Mail("snail@snailmail.com", "me@snailmail.com", "Hey", "I have a shell"),
-                new Mail("slug@snailmail.com", "me@snailmail.com", "Hey", "I am a slug"),
-                new Mail("clam@snailmail.com", "me@snailmail.com", "Hey", "...")
+                new Mail("snail@snailmail.com", "Hey", "me@snailmail.com", "I am a snail"),
+                new Mail("snail@snailmail.com", "Hey", "me@snailmail.com", "I have a shell"),
+                new Mail("slug@snailmail.com", "Hey", "me@snailmail.com", "I am a slug"),
+                new Mail("clam@snailmail.com", "Hey", "me@snailmail.com", "...")
         );
 
         //Easily configure and return an HTTP response thanks to ResponseEntity
+        //200 level status code, and inbox as the response body
         return ResponseEntity.ok().body(inbox);
+
+    }
+
+    //This method will take in a Mail object and send a (fake) email
+    @PostMapping
+    public ResponseEntity<Mail> sendMail(@RequestBody Mail mail) {
+
+        //Error handling to make sure it's valid mail (just a couple, to get the idea)
+        if(mail.getRecipient() == null || mail.getRecipient().isBlank()){
+            //400 level status code, and empty response body
+            return ResponseEntity.badRequest().body(null);
+        }
+        //TODO: check the other fields, and stuff like is the email address is valid
+
+        //In a real app, we'd send this deeper into the app to hit the database
+
+        //For now, we'll just return the mail to the user
+        return ResponseEntity.ok().body(mail);
 
     }
 
