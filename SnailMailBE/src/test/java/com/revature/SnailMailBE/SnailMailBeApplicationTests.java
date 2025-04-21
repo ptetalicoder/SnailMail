485e9ff990cf5683487f35a99e43bfd894045d35
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-04-21 11:05:23 -0400

    backend initial commit

---
+++ SnailMailBE/src/test/java/com/revature/SnailMailBE/SnailMailBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.SnailMailBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailMailBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
