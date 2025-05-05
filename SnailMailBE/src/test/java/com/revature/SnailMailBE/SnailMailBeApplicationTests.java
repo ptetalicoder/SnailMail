7e086cc6555cf02d4bec626bf2f0c5554c79785f
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-05-05 14:26:29 -0400

    Setup RestAssured in SnailMail, wrote one positive test

--- SnailMailBE/src/test/java/com/revature/SnailMailBE/SnailMailBeApplicationTests.java
+++ SnailMailBE/src/test/java/com/revature/SnailMailBE/SnailMailBeApplicationTests.java
@@ -1,7 +1,11 @@
 package com.revature.SnailMailBE;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 class SnailMailBeApplicationTests {
@@ -9,5 +13,25 @@ class SnailMailBeApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	//First RestAssured test - Just makes sure getInbox() works as expected
+	@Test
+	void testGetInbox() {
+
+		//Response object from RestAssured. Lets us extract and test HTTP Responses
+		Response response = given()
+				.when().get("http://localhost:8080/mail")
+				.then().extract().response();
+
+		//Now we can run assertions on the response
+		response.then()
+				.statusCode(200)
+				.body("size()", greaterThan(0))
+				.body("[0].sender", notNullValue())
+				.body("[0].recipient", equalTo("me@snailmail.com"));
+	}
+
+	//NOTE: We used JUnit, which is great and for Java tests in general...
+	//But RestAssured is built SPECIFICALLY for testing REST APIs
 
 }
